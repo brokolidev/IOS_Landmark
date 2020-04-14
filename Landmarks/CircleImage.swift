d6238561ac11659cce6fe46eebbd067ce9a5fb6e
brokoli <bocalist@gmail.com>
2020-04-14 22:37:03 +0900

    add another swiftui view for croped image

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Hyunseung Choi on 2020/04/14.
+//  Copyright © 2020 Hyunseung Choi. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
